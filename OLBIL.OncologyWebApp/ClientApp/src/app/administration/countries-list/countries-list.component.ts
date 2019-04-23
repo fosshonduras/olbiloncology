86675581ec4e81dfb3c8fbb5f2153ba960590712
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2019-04-23 07:39:20 -0600

    Adding list and edit views for countries

--- OLBIL.OncologyWebApp/ClientApp/src/app/administration/countries-list/countries-list.component.ts
+++ OLBIL.OncologyWebApp/ClientApp/src/app/administration/countries-list/countries-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LinkRendererComponent } from '../../helper-components/LinkRendererComponent';
+import { ColDef, GridOptions } from 'ag-grid-community';
+import { CountryModel, CountriesClient } from '../../api-clients';
 
 @Component({
   selector: 'app-countries-list',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countries-list.component.css']
 })
 export class CountriesListComponent implements OnInit {
+  isLoading: boolean = false;
+  rowData: CountryModel[] = [];
 
-  constructor() { }
+  defaultColDef: ColDef = {
+    resizable: true
+  };
+
+  gridOptions: GridOptions = {};
+  columnDefs: ColDef[] = [
+    { headerName: 'País ID', field: 'countryId' },
+    {
+      headerName: 'ISO2', field: 'isoCode2',
+      cellRendererFramework: LinkRendererComponent,
+      cellRendererParams: ({ data }) => {
+        return ({
+          inRouterLink: `./`,
+          routeParam: data.countryId,
+          value: data.isoCode2
+        });
+      }
+    },
+    { headerName: 'ISO3', field: 'isoCode3' },
+    { headerName: 'Nombre (Es)', field: 'nameEs' },
+    { headerName: 'Nombre (En)', field: 'nameEn' },
+  ];
+
+  constructor(
+    private client: CountriesClient
+  ) { }
 
   ngOnInit() {
-  }
+    this.isLoading = true;
 
+    this.client.getAll()
+      .subscribe(result => {
+        this.rowData = result.items;
+        this.isLoading = false;
+        this.gridOptions.columnApi.autoSizeAllColumns();
+      }, err => {
+        console.log(err);
+      })
+  }
 }
+
--- OLBIL.OncologyWebApp/ClientApp/src/app/administration/country-edit/country-edit.component.ts
+++ OLBIL.OncologyWebApp/ClientApp/src/app/administration/country-edit/country-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CountryModel, CountriesClient } from '../../api-clients';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-country-edit',
@@ -6,10 +9,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./country-edit.component.css']
 })
 export class CountryEditComponent implements OnInit {
+  pageTitle: string;
+  isNewRecord: boolean = false;
+  countryId: number = -1;
+  country: CountryModel;
+  isSaving: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService,
+    private client: CountriesClient
+  ) {
+    this.country = new CountryModel();
   }
 
+  ngOnInit() {
+    let id = this.route.snapshot.paramMap.get("countryId");
+
+    if (id === "new" || id === null) {
+      this.isNewRecord = true;
+    } else {
+      this.countryId = +id;
+    }
+
+    this.pageTitle = `${this.isNewRecord ? "Nuevo" : "Edición de"} País`;
+
+    this.setupTargetRecord();
+  }
+
+
+  setupTargetRecord(): any {
+    if (!this.isNewRecord) {
+      this.client.getCountry(this.countryId)
+        .subscribe(result => {
+          this.country = result;
+        }, err => {
+          this.toastr.warning(err);
+        });
+    }
+  }
+
+  submitRegistration(regForm) {
+    this.isSaving = true;
+
+    if (this.isNewRecord) {
+      this.client.createCountry(this.country)
+        .subscribe(r => this.handleSuccess(r), e => this.handleFailure(e));
+    } else {
+      this.client.updateCountry(this.country)
+        .subscribe(r => this.handleSuccess(r), e => this.handleFailure(e));
+    }
+  }
+
+  private handleFailure(err: any) {
+    this.isSaving = false;
+    this.toastr.warning(JSON.parse(err.response).error);
+  }
+
+  private handleSuccess(result) {
+    this.toastr.success("Datos guardados.");
+    setTimeout(() => {
+      this.router.navigate(['./countries'], { relativeTo: this.route.parent });
+    }, 2000);
+  }
 }
+
